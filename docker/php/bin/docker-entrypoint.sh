96db0ecedafbe8f299500507a7b9bdbfc638a980
Jan Thomas <jan.thomas@rwth-aachen.de>
2020-06-07 11:43:05 +0200

    Remove memory limit for composer install

--- docker/php/bin/docker-entrypoint.sh
+++ docker/php/bin/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /var/www/project
-COMPOSER_MEMORY_LIMIT=2G composer install
+COMPOSER_MEMORY_LIMIT=-1 composer install
 
 CONTAINER_STARTED=bin/container-started.sh
 if test -f "$CONTAINER_STARTED"; then
