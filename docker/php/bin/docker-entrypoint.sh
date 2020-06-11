49593418b9de06f958904ecd491d723ede0eca96
Jan Thomas <jan.thomas@rwth-aachen.de>
2020-06-11 13:26:23 +0200

    Make memory limit and time zone configurable at container runtime

--- docker/php/bin/docker-entrypoint.sh
+++ docker/php/bin/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Set timezone
+ln -snf /usr/share/zoneinfo/${PHP_TIMEZONE} /etc/localtime
+echo ${PHP_TIMEZONE} > /etc/timezone
+
+# Install dependencies
 cd /var/www/project
 COMPOSER_MEMORY_LIMIT=-1 composer install
 
+# Invoke custom container started hook
 CONTAINER_STARTED=bin/container-started.sh
 if test -f "$CONTAINER_STARTED"; then
     source $CONTAINER_STARTED
